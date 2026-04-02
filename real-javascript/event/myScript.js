3bafefcb3317fbce1b0ea0a0270368581fa7b151
Kevin Xie <kxie@brooksschool.org>
2026-04-02 01:10:06 -0400

    revised

--- real-javascript/event/myScript.js
+++ real-javascript/event/myScript.js
@@ -5,12 +5,9 @@ const message = document.getElementById("message")
 
 button.addEventListener("click", function () {
   const name = nameInput.value
-  const animal = animalInput.value;
+  const animal = animalInput.value
+  message.innerHTML = "Hello " + name + ". Don't you wish you were a " + animal + "?"})
 
-  message.innerHTML = "Hello " + name + ". Don't you wish you were a " + animal + "?"});
+button.addEventListener("mouseover", changeColor)
 
-button.addEventListener("mouseover", changeColor);
-
-function changeColor() {
-  nameInput.style.backgroundColor = "red";
-}
+function changeColor() {nameInput.style.backgroundColor = "red"}
